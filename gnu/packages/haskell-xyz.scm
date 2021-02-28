28d4752c16e2661ac716a946eccb7af5381e9bb4
prilcy_yellow <prilcy_yellow@dismail.de>
2021-02-28 09:57:23 +0700

    Update ghc-uuid-types

--- gnu/packages/haskell-xyz.scm
+++ gnu/packages/haskell-xyz.scm
@@ -14617,16 +14617,9 @@ parsing and printing @dfn{Universally Unique Identifiers} or UUIDs.")
          "1zdka5jnm1h6k36w3nr647yf3b5lqb336g3fkprhd6san9x52xlj"))))
     (build-system haskell-build-system)
     (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-before 'configure 'strip-test-framework-constraints
-           (lambda _
-             (substitute* "uuid-types.cabal"
-               (("HUnit >=1\\.2 && < 1\\.4") "HUnit")
-               (("QuickCheck >=2\\.4 && < 2\\.9") "QuickCheck")
-               (("tasty >= 0\\.10 && < 0\\.12") "tasty")
-               (("tasty-hunit == 0\\.9\\.\\*") "tasty-hunit")
-               (("tasty-quickcheck == 0\\.8\\.\\*") "tasty-quickcheck")))))))
+     `(#:cabal-revision
+       ("4"
+        "0ipwfd5y8021ygpadjjhchw5irm0x27dlv1k2hf4znza5v7hadcn")))
     (inputs `(("ghc-hashable" ,ghc-hashable)
               ("ghc-random" ,ghc-random)))
     (native-inputs `(("ghc-hunit" ,ghc-hunit)
